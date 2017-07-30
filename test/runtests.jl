8cb59b7550ce42c6d3d9d52112194a99857388f2
Alexander Belopolsky <a@enlnt.com>
2017-07-30 10:35:27 -0400

    Organized tests in testsets.

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,10 +8,13 @@ function roundtrip_scalar(jk, kj, x)
     r0(k)
     return x == r
 end
-
-@test roundtrip_scalar(xg, kg, I_(42))
-@test roundtrip_scalar(xh, kh, I_(101))
-@test roundtrip_scalar(xi, ki, I_(10101))
-@test roundtrip_scalar(xj, kj, J_(1010101))
-@test roundtrip_scalar(xf, kf, F_(1e10))
-@test roundtrip_scalar(xs, ks, "abc")
+@testset "Low level (k)" begin
+  @testset "Scalar roundtrip" begin
+    @test roundtrip_scalar(xg, kg, I_(42))
+    @test roundtrip_scalar(xh, kh, I_(101))
+    @test roundtrip_scalar(xi, ki, I_(10101))
+    @test roundtrip_scalar(xj, kj, J_(1010101))
+    @test roundtrip_scalar(xf, kf, F_(1e10))
+    @test roundtrip_scalar(xs, ks, "abc")
+  end
+end
