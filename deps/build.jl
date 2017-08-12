6b061a5a3f6da36b4e30acfe67601adbdfcb3bda
Alexander Belopolsky <a@enlnt.com>
2017-08-12 12:24:30 -0400

    ENH #6 Install server components during the package build.

---
+++ deps/build.jl
@@ -0,0 +1,15 @@
+println("Building JuQ...")
+const OS = Dict(:Darwin => 'm', :Linux => 'l')
+QARCH = string(OS[Sys.KERNEL], Sys.WORD_SIZE)
+QHOME = get(ENV, "QHOME", "")
+if QHOME == ""
+  QHOME = joinpath(homedir(), "q")
+end
+EMBED_DIR = joinpath(@__DIR__, "..", "embed")
+if isdir(joinpath(QHOME, QARCH))
+  run(`make -C $(EMBED_DIR) QHOME=$(QHOME) QARCH=$(QARCH) install`)
+  println("Installed server components for $(QARCH) in $(QHOME).")
+else
+  println("Could not find a suitable kdb+ installation.")
+  println("Server components are not installed")
+end
