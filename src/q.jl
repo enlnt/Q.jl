a448c3767dce359f3384b9f6cb805db752d24ddf
Alexander Belopolsky <abalkin@users.noreply.github.com>
2017-08-29 15:56:45 -0400

    TST Added a simple test for q.jl code.

--- src/q.jl
+++ src/q.jl
@@ -2,7 +2,10 @@ export @q_cmd
 macro q_cmd(s) _E(k(0, s)) end
 function Base.show(io::IO, x::K)
     s = k(0, "{` sv .Q.S[40 80;0;x]}", r1(x.o.x))
-    write(io, strip(unsafe_string(pointer(s))))
-    r0(s)
+    try
+        write(io, strip(unsafe_string(pointer(s), xn(s))))
+    finally
+        r0(s)
+    end
     nothing
 end
