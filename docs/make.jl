cfd854b1b52824f3682b2326cdd8f1b42614dcdc
Alexander Belopolsky <a@enlnt.com>
2017-08-30 21:26:28 -0400

    DOC BLD Deploy docs from a linux build.

--- docs/make.jl
+++ docs/make.jl
@@ -22,5 +22,5 @@ deploydocs(
     deps   = Deps.pip("mkdocs", "python-markdown-math"),
     repo = "github.com/abalkin/JuQ.jl.git",
     julia  = "0.6",
-    osname = "osx"
+    osname = "linux",
 )
