fada9eb339a35a98665d5f067fe1b85bd984cae6
Alexander Belopolsky <abalkin@users.noreply.github.com>
2017-09-01 16:56:16 -0400

    More tests (#35)
    * TST Added server-side tests.
    
    * FIX Fixed an error from K[..] in a nightly Julia build.
    
    * TST Added server-side tests for dot_ and ee.

--- test/server-tests.jl
+++ test/server-tests.jl
@@ -1,20 +1,47 @@
-@testset "server-side asarray" begin
-   @test begin
-       f = k(0, "{}")
-       a = asarray(f)
-       length(a) == 9 && unsafe_string(kS(a[2])[1]) == "x"
-   end
-   @test asarray(k(0, "*:"))[] == 3
-   @test asarray(k(0, "*"))[] == 3
-   @test asarray(k(0, "(/:)"))[] == 4
-   @test (a = asarray(k(0, "{z}[10;20]")); asarray(a[2], false)[] == 10)
-   @test (a = asarray(k(0, "('[sum;*])")); asarray(a[2], false)[] == 3)
-   @testset "f<adverb>" for c in "'/\\"
-       @test (a = asarray(k(0, "*$c")); asarray(a[], false)[] == 3)
-       @test (a = asarray(k(0, "*$(c):")); asarray(a[], false)[] == 3)
-   end
-   @test (a = asarray(k(0, ".J.jl.init")); eltype(a) == Ptr{V_})
+qa(cmd) = asarray(k(0, cmd))
+qa(cmd, x...) = asarray(k(0, cmd), map(K_new, x)...)
+@testset "server-side low level" begin
+  @test begin  # dot_
+    f = k(0, "til")
+    af = asarray(f)  # ensure r0
+    x = knk(1, kj(3))
+    ax = asarray(x)  # ensure r0
+    asarray(dot_(f, x)) == [0, 1, 2]
+  end
+  @test begin  # ee
+    f = k(0, "til")
+    af = asarray(f)    # ensure r0
+    x = knk(1, kc(99)) # ,"c"
+    ax = asarray(x)    # ensure r0
+    unsafe_string(asarray(ee(dot_(f, x)))[]) == "type"
+  end
 end
+@testset "server-side asarray" begin
+  @test begin
+      f = k(0, "{}")
+      a = asarray(f)
+      length(a) == 9 && unsafe_string(kS(a[2])[1]) == "x"
+  end
+  @test asarray(k(0, "*:"))[] == 3
+  @test asarray(k(0, "*"))[] == 3
+  @test asarray(k(0, "(/:)"))[] == 4
+  @test (a = asarray(k(0, "{z}[10;20]")); asarray(a[2], false)[] == 10)
+  @test (a = asarray(k(0, "('[sum;*])")); asarray(a[2], false)[] == 3)
+  @testset "f<adverb>" for c in "'/\\"
+      @test (a = asarray(k(0, "*$c")); asarray(a[], false)[] == 3)
+      @test (a = asarray(k(0, "*$(c):")); asarray(a[], false)[] == 3)
+  end
+  @test (a = asarray(k(0, ".J.jl.init")); eltype(a) == Ptr{V_})
+  @test cdtemp() do  # enum
+    qa("`:a set `:sym?`a`b`c")
+    qa("get`:a") == [0, 1, 2]
+  end
+  @test cdtemp() do  # nested
+    qa("`:a set (1 2;3 4 5)")
+    qa("get`:a") == [2, 5]  # breaks
+  end
+end
+
 @testset "q commands" begin
   @test q`+`(1, 2) == 3
   @test q`til 3`() == q`til`(3) == [0, 1, 2]
