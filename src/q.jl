005a7a06fdca2030adcd666563cea250b7a00e5e
Alexander Belopolsky <a@enlnt.com>
2017-09-01 17:51:35 -0400

    MNT Reimplemented scalars using asarray().
    MNT Reimplemented vectors using asarray().
    
    MNT Reimplemented K_Table using asarray().
    
    DOC Documented KdbException.
    
    MNT Reimplemented K_Other to use asarray().
    
    MNT Removed unused code.
    
    TST Fixed qa(cmd, x...).
    
    ENH Implemented K_new(K).
    
    ENH Introduced K_Lambda to represent composite callables.
    
    K_Lambda is used to represent q lambdas (t=100), projections
    (t=104) and compositions (t=105).
    
    ENH Defined conversion from K_real to Float64.
    
    MNT Rewrote push! using struct definition and fieldoffset().
    
    DOC Removed K_Object from docs.
    
     - added KdbException
    
    MNT Removed unused code.
    
    MNT Removed low level iteration in favor of asarray().
    
    TST Simplified tests.
    
    TST Added server-side tests.
    
    MNT Removed unused code.
    
    TST Added a script to process coverage.

--- src/q.jl
+++ src/q.jl
@@ -1,15 +1,15 @@
 export @q_cmd
+# TODO: Use dot_ here.
+_apply(f, args...) = K(k(0, ".", K_new(f), K_new(args)))
+(f::K_Lambda)() = f(nothing)
+(f::K_Other)() = f(nothing)
+(f::K_Lambda)(args...) = _apply(f, args...)
+(f::K_Other)(args...) = _apply(f, args...)
 
-(f::JuQ.K_Other)() = f(nothing)
-function (f::JuQ.K_Other)(args...)
-   p = k(0, ".", K_new(f.o.x), K_new(args))
-   K(p)
-end
-
-const Q_PARSE = "{\$[0=t:type e:parse x;{y;eval x}e;t=-11;eval e;e]}" 
+const Q_PARSE = "{\$[0=t:type e:parse x;{y;eval x}e;t=-11;eval e;e]}"
 macro q_cmd(s) _E(k(0, Q_PARSE, kp(s))) end
 function Base.show(io::IO, x::K)
-    s = k(0, "{` sv .Q.S[40 80;0;x]}", r1(x.o.x))
+    s = k(0, "{` sv .Q.S[40 80;0;x]}", K_new(x))
     try
         write(io, strip(unsafe_string(pointer(s), xn(s))))
     finally
--- src/table.jl
+++ src/table.jl
@@ -1,22 +1,21 @@
 using DataFrames
 
 struct K_Table  <: AbstractDataFrame
-    o::K_Object
+    a::Array{K_,0}
     function K_Table(x::K_)
-        o = K_Object(x)
+        a = asarray(x)
         t = xt(x)
         if(t != XT)
             throw(ArgumentError("type mismatch: t=$t ≠ $XT"))
         end
-        return new(o)
+        return new(a)
     end
     function K_Table(columns::Vector, colnames::Vector{Symbol})
         ncols = length(columns)
-        x = K(colnames)
-        kcols = map(x->r1(K(x).o.x), columns)
-        y = knk(ncols, kcols...)
-        o = K_Object(xT(xD(r1(x.o.x), y)))
-        new(o)
+        x = K_new(colnames)
+        y = K_new(columns)
+        a = asarray(xT(xD(x, y)))
+        new(a)
     end
     function K_Table(; kwargs...)
         x = ktn(KS, 0)
@@ -26,16 +25,16 @@ struct K_Table  <: AbstractDataFrame
             x = js(rx, ss(k))
             y = jk(ry, K_new(v))
         end
-        o = K_Object(xT(xD(x, y)))
-        new(o)
+        a = asarray(xT(xD(x, y)))
+        new(a)
     end
 end
 
-DataFrames.ncol(x::K_Table) = xn(xx(xk(x.o.x)))
-DataFrames.nrow(x::K_Table) = xn(unsafe_load(Ptr{K_}(xy(xk(x.o.x))+16)))
-DataFrames.index(x::K_Table) = DataFrames.Index(Array(K(xx(xk(x.o.x)))))
-DataFrames.columns(x::K_Table) = K(xx(xk(x.o.x)))
-cols(x::K_Table) = K(xx(xk(x.o.x)))
+DataFrames.ncol(x::K_Table) = xn(xx(x.a[]))
+DataFrames.nrow(x::K_Table) = xn(unsafe_load(Ptr{K_}(xy(x.a[])+16)))
+DataFrames.index(x::K_Table) = DataFrames.Index(Array(K(xx(x.a[]))))
+DataFrames.columns(x::K_Table) = K(xx(x.a[]))
+cols(x::K_Table) = K(xx(x.a[]))
 Base.getindex(x::K_Table, i::Integer) =
-    K(r1(unsafe_load(Ptr{K_}(xy(xk(x.o.x))+16), i)))
+    K(r1(unsafe_load(Ptr{K_}(xy(x.a[])+16), i)))
 Base.getindex(x::K_Table, i::Integer, j::Integer) = x[j][i]
