148358098489801f8cc47bd586d36d7e89d41ab7
Alexander Belopolsky <a@enlnt.com>
2017-09-03 15:16:48 -0400

    TST Added tests
    Also added a few promotion rules.

--- src/promote_rules.jl
+++ src/promote_rules.jl
@@ -1,5 +1,5 @@
 # TODO: These rules are incomplete - need to figure out how
-# to mimic Julia's builtin promotion rules. 
+# to mimic Julia's builtin promotion rules.
 Base.promote_rule(::Type{K_real}, ::Type{Int64}) = Float64
 Base.promote_rule(::Type{K_real}, ::Type{Int32}) = Float32
 
@@ -15,3 +15,16 @@ Base.promote_rule(::Type{K_int}, ::Type{Int64}) = Int64
 Base.promote_rule(::Type{K_short}, ::Type{Float64}) = Float64
 Base.promote_rule(::Type{K_short}, ::Type{Int64}) = Int64
 Base.promote_rule(::Type{K_short}, ::Type{Int32}) = Int32
+
+# K - K rules
+Base.promote_rule(::Type{K_float}, ::Type{K_float}) = Float64
+Base.promote_rule(::Type{K_float}, ::Type{K_real}) = Float64
+Base.promote_rule(::Type{K_float}, ::Type{K_int}) = Int64
+Base.promote_rule(::Type{K_float}, ::Type{K_short}) = Int64
+Base.promote_rule(::Type{K_float}, ::Type{K_boolean}) = Int64
+
+Base.promote_rule(::Type{K_long}, ::Type{K_float}) = Float64
+Base.promote_rule(::Type{K_long}, ::Type{K_real}) = Float64
+Base.promote_rule(::Type{K_long}, ::Type{K_int}) = Int64
+Base.promote_rule(::Type{K_long}, ::Type{K_short}) = Int64
+Base.promote_rule(::Type{K_long}, ::Type{K_boolean}) = Int64
