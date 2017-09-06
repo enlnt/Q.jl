2a4760b7723dfd3cc0d86cd339e9639bd5bd1eff
Aleks Bunin <github@compuix.com>
2017-09-06 11:11:48 -0400

    Update make.jl.

--- docs/make.jl
+++ docs/make.jl
@@ -4,7 +4,7 @@ using Q
 
 makedocs(
     modules   = [Q, Q._k],
-    sitename  = "Q - Julia for kdb+",
+    sitename  = "Q.jl - Julia for kdb+",
     linkcheck = true,
     pages = [
         "Home" => "index.md",
@@ -19,7 +19,7 @@ makedocs(
 
 deploydocs(
     deps   = Deps.pip("mkdocs", "python-markdown-math"),
-    repo = "github.com/abalkin/Q.jl.git",
+    repo = "github.com/enlnt/Q.jl.git",
     julia  = "0.6",
     osname = "linux",
 )
