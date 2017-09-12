26deb04db7c3a74e0a5d2c7f78f3632d9922d5bd
Alexander Belopolsky <a@enlnt.com>
2017-09-12 13:18:31 -0400

    MNT Generate julia config settings at build time.

--- deps/build.jl
+++ deps/build.jl
@@ -1,15 +1,16 @@
-println("Building Q...")
 const OS = Dict(:Darwin => 'm', :Linux => 'l')
-QARCH = string(OS[Sys.KERNEL], Sys.WORD_SIZE)
-QHOME = get(ENV, "QHOME", "")
-if QHOME == ""
-  QHOME = joinpath(homedir(), "q")
+const QARCH = string(OS[Sys.KERNEL], Sys.WORD_SIZE)
+try
+  global const QHOME = ENV["QHOME"]
+catch
+  global const QHOME = joinpath(homedir(), "q")
 end
-EMBED_DIR = joinpath(@__DIR__, "..", "embed")
+const EMBED_DIR = joinpath(@__DIR__, "..", "embed")
 if isdir(joinpath(QHOME, QARCH))
   run(`make -C $(EMBED_DIR) QHOME=$(QHOME) QARCH=$(QARCH) install`)
-  println("Installed server components for $(QARCH) in $(QHOME).")
+  run(`make -C $(EMBED_DIR) QHOME=$(QHOME) QARCH=$(QARCH) clean`)
+  info("Installed server components for $(QARCH) in $(QHOME).")
 else
-  println("Could not find a suitable kdb+ installation.")
-  println("Server components are not installed")
+  warn("Could not find a suitable kdb+ installation.",
+       "\nServer components are not installed.")
 end
