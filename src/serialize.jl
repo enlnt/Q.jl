c84150260cf7318b6b6570887b01d7d29d792183
Alexander Belopolsky <a@enlnt.com>
2017-09-27 19:52:42 -0400

    MNT Optimized serialization.

--- src/serialize.jl
+++ src/serialize.jl
@@ -1,16 +1,18 @@
 struct K_Mark end
 
-function _serialize(io::AbstractSerializer, x, T::Type)
-    Base.serialize_type(io, K_Mark)
-    serialize(io, T)
+function _serialize(s::AbstractSerializer, x, T::Type)
+    Base.serialize_type(s, K_Mark)
+    serialize(s, T)
     # 3 - unenumerate, compress, allow serialization of timespan and timestamp
     b = K(b9(3, kpointer(x)))
-    # TODO: Store count+bytes instead of serializing a vector.
-    serialize(io, b.a)
+    write(s.io, Int64(length(b)))
+    write(s.io, b.a)
 end
 
-function Base.deserialize(io::AbstractSerializer, ::Type{K_Mark})
-    T = deserialize(io)
-    b = K(deserialize(io))
+function Base.deserialize(s::AbstractSerializer, ::Type{K_Mark})
+    T = deserialize(s)
+    n = read(s.io, Int64)
+    b = _vector(KG, n)
+    readbytes!(s.io, b)
     T(d9(kpointer(b)))
 end
