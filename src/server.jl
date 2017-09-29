3e6286148bcefde5e756276bbfe8507ed7999b23
Alexander Belopolsky <a@enlnt.com>
2017-09-29 00:01:30 -0400

    ENH Implemented basic event loop methods.

--- src/server.jl
+++ src/server.jl
@@ -192,7 +192,19 @@ const res = [b100; b101; b102; b104]
     _Q() = new($([K(k(0, rstrip(x, ['_']))) for x in res]...))
 end
 
-
+function process_julia_events(d::I_)
+    # NB: process_events(false) calls jl_process_events(loop)
+    # which in turn calls uv_run(loop, UV_RUN_NOWAIT).
+    k(0, "1 enlist\".\"")
+    Base.process_events(false)
+    0
+end
+const process_julia_events_c = cfunction(process_julia_events, Int, (I_, ))
+function start_julia()
+    loop = Base.eventloop()
+    d = ccall(:uv_backend_fd, Cint, (Ptr{Void}, ), loop)
+    r0(sd1(d, process_julia_events_c))
+end
 # function Base.show(io::IO, x::Union{K_Other,K_Lambda})
 #     s = k(0, "{` sv .Q.S[40 80;0;x]}", K_new(x))
 #     try
