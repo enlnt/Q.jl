999e105e752e095793bd73e06e384acc6ae3d5a7
Alexander Belopolsky <a@enlnt.com>
2017-09-30 12:27:50 -0400

    WIP Near-working async frontend.
    Start with
    
        q embed/jl.q -p 8888 </dev/null
    
      This reopens the TTY and presents a working Julia session. The
      kdb+ process accepts connections but any command entered in qcon
      results in a segfault.

--- embed/jl.q
+++ embed/jl.q
@@ -1,3 +1,7 @@
+\l julia-debug-config.q
 J)import Q
+/ J)Q.reopen_stdin()
+/ J)Base._start()
+J)Q.start_julia()
 J)Q.reopen_stdin()
-J)Base._start()
+J)task = @async Base._start()
--- src/async.jl
+++ src/async.jl
@@ -1,17 +1,36 @@
 function process_julia_events(d::I_)
     # NB: process_events(false) calls jl_process_events(loop)
     # which in turn calls uv_run(loop, UV_RUN_NOWAIT).
-    k(0, "1 enlist\".\"")
+    #k(0, "1 enlist\".\"")
+    n = -1
     try
-        Base.process_events(false)
+        n = Base.process_events(false)
     catch err
         println(err)
     end
+    #k(0, "1 string $n")
+    write(notification_pipe.in, 'e')
+    ccall(:jl_breakpoint, Void, (Any,), d)
     0
 end
 const process_julia_events_c = cfunction(process_julia_events, Int, (I_, ))
+
+function process_julia_notification(d::I_)
+    c = read(notification_pipe.out, 1)
+    #k(0, "1 enlist\"$c\"")
+    process_julia_events(d)
+end
+const process_julia_notification_c = cfunction(process_julia_notification,
+    Int, (I_, ))
+
+const notification_pipe = Pipe()
 function start_julia()
     loop = Base.eventloop()
     d = ccall(:uv_backend_fd, Cint, (Ptr{Void}, ), loop)
     r0(sd1(d, process_julia_events_c))
+    Base.link_pipe(notification_pipe)
+    write(notification_pipe.in, 's')
+    d = Base._fd(Q.notification_pipe.out).fd
+    r0(sd1(d, process_julia_notification_c))
+    ccall(:jl_breakpoint, Void, (Any,), notification_pipe)
 end
