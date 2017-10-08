593a548ed0bf6f19a8caaa69aa830d6836d9b181
Alexander Belopolsky <a@enlnt.com>
2017-10-08 18:15:27 -0400

    ENH Added client-side dot_(f, n).

--- src/impl.jl
+++ src/impl.jl
@@ -10,6 +10,17 @@ function impl_dl(f::Ptr{V_}, n::Integer)
     unsafe_store!(Ptr{Ptr{V_}}(x+16), f)
     x
 end
+_dot(f::Ptr{V_}, x1) = ccall(f, K_, (K_, ), x1)
+_dot(f::Ptr{V_}, x1, x2) = ccall(f, K_, (K_, K_), x1, x2)
+_dot(f::Ptr{V_}, x1, x2, x3) = ccall(f, K_, (K_, K_, K_), x1, x2, x3)
+_dot(f::Ptr{V_}, x1, x2, x3, x4) = ccall(f, K_,
+    (K_, K_, K_, K_), x1, x2, x3, x4)
+function impl_dot(f::K_, x::K_)
+    p = unsafe_load(Ptr{Ptr{V_}}(f+16))
+    n = unsafe_load(f).u
+    @assert xn(x) == n  # XXX: Projections are nyi.
+    _dot(p, kK(x)...)
+end
 function impl_khp(h::Cstring, p::Cint)
     x = k(0, "hopen", ks(string(":", unsafe_string(h), ":", p)))
     x == K_NULL ? Cint(-1) : xi(x)
--- test/impl-tests.jl
+++ test/impl-tests.jl
@@ -2,12 +2,18 @@ using Base.Test
 using Q._k
 import Q: K_Ref
 import Q._k: impl_dl, impl_dot
-f(x, y) = kj(xj(x) + xj(y))
+fun(x, y) = kj(xj(x) + xj(y))
 @testset "impl" begin
     @test begin
-        x = K_Ref(impl_dl(cfunction(f, K_, (K_, K_)), 2))
+        x = K_Ref(impl_dl(cfunction(fun, K_, (K_, K_)), 2))
         a = K_Ref(knk(2, kj(2), kj(3)))
         r = K_Ref(dot_(x.x, a.x))
+        xj(r.x) == 5
+    end
+    @test begin
+        g = K_Ref(impl_dl(cfunction(fun, K_, (K_, K_)), 2))
+        a = K_Ref(knk(2, kj(2), kj(3)))
+        r = K_Ref(impl_dot(g.x, a.x))
         xj(r.x) == 5
     end
 end
